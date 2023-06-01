252fbc4f01bd5585e4a14d3d3640a9a2aa32bfd5
martha danladi <marthadanladi>
2023-06-01 12:11:11 +0100

    problem solving

--- src/chapterFour/LargestNumber.java
+++ src/chapterFour/LargestNumber.java
@@ -6,16 +6,23 @@ public class LargestNumber {
     public static void main(String[] args) {
         int counter = 0;
         int largest = 0;
+        int secondLargest = 0;
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        int number = input.nextInt();
 
-        while (counter< 9) {
-            if(number>largest) largest = number;
-            counter++;
+
+        while (counter< 10) {
             System.out.println("Enter a number: ");
-            number = input.nextInt();
+            int number = input.nextInt();
+            input.nextLine();
+
+            if(number>largest){
+                secondLargest= largest;
+                largest = number;
+            }
+            else if(number>secondLargest) secondLargest=number;
+            counter++;
         }
         System.out.println("Largest: "+largest);
+        System.out.println("Second: "+secondLargest);
     }
 }
--- src/chapterFour/TabularOutput.java
+++ src/chapterFour/TabularOutput.java
@@ -3,9 +3,13 @@ package chapterFour;
 public class TabularOutput {
     public static void main(String[] args) {
         System.out.println("N       N2      N3      N4");
+
         for (int i = 1; i < 6; i++) {
-
-
+            for(int j = 0; j < i; j++) {
+                System.out.printf("%d %8d %8d %8d",i, i*i, i*i*i, i*i*i*i);
+                break;
+            }
+            System.out.println();
         }
     }
 }
