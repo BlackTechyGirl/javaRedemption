6dd1ff536fcddf5a0b3ef85c37e0393610cb2219
martha danladi <marthadanladi>
2023-06-06 15:14:56 +0100

    chapter6 started

--- src/chapterSix/RoundingNumbers.java
+++ src/chapterSix/RoundingNumbers.java
@@ -5,6 +5,14 @@ public class RoundingNumbers {
         for (int i = 0; i < 10; i++) {
             System.out.println(i +" -> "+ Math.floor(i+0.5));
         }
+
+
+
+
+        for (int i = 0; i <10 ; i++) {
+            double rounded = Math.floor(i);
+            System.out.println(i+" -> "+rounded+" -> "+rounded/10+" -> "+rounded/100+" -> "+rounded/1000);
+        }
     }
 
 }
