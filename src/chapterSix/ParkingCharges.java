d8663bee3865fd327695e81cf445e7b15946322b
martha danladi <marthadanladi>
2023-06-06 15:06:31 +0100

    chapter6 started

--- src/chapterSix/ParkingCharges.java
+++ src/chapterSix/ParkingCharges.java
@@ -4,6 +4,25 @@ import java.util.Scanner;
 
 public class ParkingCharges {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double totalReceipts = 0.0;
+
+        System.out.print("Enter the number of customers: ");
+        int numCustomers = scanner.nextInt();
+
+        for (int i = 1; i <= numCustomers; i++) {
+            System.out.print("Enter the hours parked for customer " + i + ": ");
+            int hoursParked = scanner.nextInt();
+            double charge = calculateCharges(hoursParked);
+            totalReceipts += charge;
+
+            System.out.printf("Customer %d: $%.2f%n", i, charge);
+        }
+
+        System.out.printf("Total receipts for yesterday: $%.2f%n", totalReceipts);
+    }
+
     public static double calculateCharges(int hours) {
         double minimumFee = 2.00;
         double additionalFee = 0.5;
---
+++ src/chapterSix/RoundingNumbers.java
@@ -0,0 +1,10 @@
+package chapterSix;
+
+public class RoundingNumbers {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i +" -> "+ Math.floor(i+0.5));
+        }
+    }
+
+}
